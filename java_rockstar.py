f2994d94090c7a7b5bc1c97f982b17df6b672adf
Ashif Ismail <cyber2maro@gmail.com>
2018-01-25 19:23:05 +0530

    Delete java_rockstar.py

--- java_rockstar.py
+++
@@ -1,5 +0,0 @@
-from rockstar import RockStar
-
-java_code = "System.out.println('Hello cruel wolrd')"
-rock_it_bro = RockStar(days=777, file_name='hello.java', code=java_code)
-rock_it_bro.make_me_a_rockstar()
